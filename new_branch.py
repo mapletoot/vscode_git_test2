3439f82790be2850b1348f6e549404849201350e
mapletoot <149334794+mapletoot@users.noreply.github.com>
2023-12-30 13:23:59 +0000

    Second commit New Branch

--- new_branch.py
+++ new_branch.py
@@ -1,1 +1,3 @@
-# I'm just going to work on this in my new branch and leave the other files alone.
+# I'm just going to work on this in my new branch and leave the other files alone
+
+# when there is something to add it appears in the git icon page but only when it's been saved
